e07b873245b26ebe94d69643b837ef3939196b03
Carson <jackparadise520a@gmail.com>
2019-08-27 14:37:47 +0800

    quick build sphinx_html with build.py

--- localization.bat
+++ localization.bat
@@ -6,7 +6,7 @@ REM source/{locale/{language}}
 REM Create .po
 
 REM sphinx-intl update -p docs/gettext  # If the language directory already exists, just using this command!
-sphinx-intl update -p docs/gettext -l zh_TW -l en -l zh-CN
+sphinx-intl update -p docs/gettext -l zh_TW -l en
 REM sphinx-intl update -p docs/gettext -l en  It't ok {-l multiple language}
 
 
@@ -14,5 +14,5 @@ REM Translator in ...
 
 REM 
 REM sphinx-build -E -b html source docs/zh-tw  -D language=zh_TW
-sphinx-build -b html source docs/zh-tw  -D language=zh_TW
+sphinx-build -b html source docs/zh_TW  -D language=zh_TW
 sphinx-build -b html source docs/en     -D language=en
